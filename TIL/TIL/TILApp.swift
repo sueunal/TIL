eae8eb377b7dd604cbf17f6f7a52e0cfc31edf6d
sueunal <doms@kakao.com>
2023-11-30 13:37:55 +0900

    [➕add]: TIL프로젝트 추가

---
+++ TIL/TIL/TILApp.swift
@@ -0,0 +1,17 @@
+//
+//  TILApp.swift
+//  TIL
+//
+//  Created by sueun kim on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct TILApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
